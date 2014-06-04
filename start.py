6550d1842ce0d3ab699f3a624f8d6a0b5fe3f340
panhzh3 <haizhipan@gmail.com>
2014-06-04 10:58:47 +0800

    update start.py

--- start.py
+++ start.py
@@ -1,5 +1,5 @@
-# -*- coding: utf-8 -*-
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 import os
 import sys
@@ -30,6 +30,5 @@ def main():
     kernel.kernel.setsche(algo)
     kernel.kernel.run()
     
-
 if __name__ == '__main__':
     main()
